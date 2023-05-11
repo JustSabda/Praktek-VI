a8a754f2fb944f8c1a44ec2b1367f1f793cc6bc6
JustSabda <dataerror13@gmail.com>
2023-05-11 22:21:31 +0700

    [Sabda] New

---
+++ Assets/Script/GD JEMBUD/New test/XxMovement.cs
@@ -0,0 +1,254 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.VFX;
+
+public class XxMovement : MonoBehaviour
+{
+    
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public float groundDrag;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    //TOP DONT CHANGE
+
+    public static XxMovement Instance { get; private set; }
+    [Header("Movement")]
+    [SerializeField] private float _curAcceleration = 2.0f;
+    [SerializeField] private float _normalAcceleration = 30;
+    [SerializeField] private float _tiredAcceleration = 20;
+    [SerializeField] public bool tiredLife;
+
+    [Header("Dash")]
+    [SerializeField] private float dashPower = 24f;
+
+
+    [Header("Egg")]
+    public GameObject egg;
+    public Vector3 eggPlace;
+
+    public Transform meeple;
+    public bool hadEgg;
+
+    [Header("Charger")]
+    protected float Timer;
+    public float delayCharging;
+    [SerializeField] bool charging;
+
+    private Animator anim;
+
+
+    //private InputAction jumpAction;
+
+    [SerializeField] private GameObject indicator;
+
+    [Header("Audio")]
+    //[SerializeField] private AudioSource audidRun;
+    //[SerializeField] private AudioSource audidWalk;
+
+    [SerializeField] private AudioClip walkMap1, walkMap2;
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        charging = false;
+        tiredLife = false;
+
+        if (SceneManager.GetActiveScene().name == ("Main"))
+        {
+            //audidWalk.clip = walkMap1;
+        }
+        if (SceneManager.GetActiveScene().name == ("Main1"))
+        {
+            //audidWalk.clip = walkMap2;
+        }
+    }
+
+    private void Update()
+    {
+        //Network
+        /*
+        if (IsOwner)
+        {
+            //Debug.Log(CameraFollow.Instance);
+            CameraFollow.Instance.player = this.transform;
+        }
+
+
+        */
+
+        meeple = transform.Find("GO_Char_Telur_Basic_001");
+
+        if (meeple == null)
+        {
+            hadEgg = false;
+            //indicator.SetActive(true);
+            tiredLife = true;
+            //corner.enabled = false;
+        }
+        else
+        {
+            hadEgg = true;
+            //indicator.SetActive(false);
+            //corner.enabled = true;
+
+        }
+
+        if (charging)
+        {
+            Timer += Time.deltaTime;
+
+            if (Timer >= delayCharging)
+            {
+                Timer = 0f;
+                var egg = GetComponentInChildren<PlayerEgg>();
+
+                egg.curPowerEgg++;
+            }
+        }
+
+        if (!tiredLife)
+        {
+            _curAcceleration = _normalAcceleration;
+        }
+        else
+        {
+            _curAcceleration = _tiredAcceleration;
+        }
+
+        //ground check IMPORTANT CHANGE
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        if (verticalInput == 0 && horizontalInput == 0)
+        {
+            //anim.SetBool("Jalan", true);
+            //audidWalk.enabled = true;
+        }
+        else
+        {
+            //anim.SetBool("Jalan", false);
+            //audidWalk.enabled = false;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && !tiredLife)
+        {
+            rb.AddForce(transform.forward * dashPower, ForceMode.Impulse);
+
+            //animation dash
+            //anim.SetBool("Dash", true);
+            Timer += Time.deltaTime;
+            //audidRun.enabled = true;
+            if (Timer >= delayCharging)
+            {
+                Timer = 0f;
+                var egg = GetComponentInChildren<PlayerEgg>();
+
+                egg.curPowerEgg--;
+            }
+        }
+        else
+        {
+            //anim.SetBool("Dash", false);
+            //audidRun.enabled = false;
+        }
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Dead Wall") && hadEgg)
+        {
+            GetComponentInParent<ParentPlayer>().Destroyed = true;
+        }
+
+        if (other.gameObject.CompareTag("Charge Zone") && hadEgg)
+        {
+            charging = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Charge Zone") && hadEgg)
+        {
+            charging = false;
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall") && hadEgg)
+        {
+            rb.AddForce(transform.forward * dashPower, ForceMode.Impulse);
+            //_rb.AddExplosionForce(50, transform.position, 10f, 3.0F);
+        }
+    }
+
+
+}
---
+++ Assets/Script/GD JEMBUD/New test/XxShoot.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using Unity.Netcode;
+using UnityEngine;
+
+public class XxShoot : MonoBehaviour
+{
+    [SerializeField] private Projectile _projectile;
+    [SerializeField] private AudioClip _spawnClip;
+    [SerializeField] private float _projectileSpeed = 700;
+    [SerializeField] private float _curCooldown = 0.5f;
+    [SerializeField] private float _cooldownNormal = 0.5f;
+    [SerializeField] private float _cooldownTired = 0.5f;
+    [SerializeField] private Transform _spawner;
+
+    private float _lastFired = float.MinValue;
+    private bool _fired;
+
+    private Rigidbody rb;
+
+    [Header("Recoil")]
+    [SerializeField] private float forceBack;
+    [SerializeField] private bool shootBack;
+
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+    [SerializeField] private Transform debugTransform;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        var Egg = GetComponentInChildren<PlayerEgg>();
+
+        var player = GetComponent<XxMovement>();
+        if (Egg == null)
+        {
+            _curCooldown = _cooldownTired;
+            player.tiredLife = true;
+        }
+        else
+        {
+            if (Egg._state == PlayerEgg.state.POWEROUT)
+            {
+                _curCooldown = _cooldownTired;
+                player.tiredLife = true;
+            }
+            else
+            {
+                _curCooldown = _cooldownNormal;
+                player.tiredLife = false;
+            }
+        }
+
+        Vector3 mouseWorldPosition = Vector3.zero;
+
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        //Transform hitTransform = null;
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            debugTransform.position = raycastHit.point;
+            mouseWorldPosition = raycastHit.point;
+            //hitTransform = raycastHit.transform;
+        }
+
+        if (Input.GetMouseButton(0) && _lastFired + _curCooldown < Time.time)
+        {
+
+
+
+            _lastFired = Time.time;
+
+            Vector3 worldAimTarget = mouseWorldPosition;
+            worldAimTarget.y = transform.position.y;
+
+            //var dir = transform.forward;
+            var dir = (mouseWorldPosition - transform.position).normalized;
+
+
+
+
+
+            if (GetComponent<XxMovement>().hadEgg == true)
+            {
+                Egg.curPowerEgg -= 1;
+            }
+
+
+            //RequestFireServerRpc(dir);
+            ExecuteShoot(dir);
+            //StartCoroutine(ToggleLagIndicator());
+        }
+    }
+
+    [ServerRpc]
+    private void RequestFireServerRpc(Vector3 dir)
+    {
+        FireClientRpc(dir);
+    }
+
+    [ClientRpc]
+    private void FireClientRpc(Vector3 dir)
+    {
+        //if (!IsOwner) ExecuteShoot(dir);
+    }
+
+    private void ExecuteShoot(Vector3 dir)
+    {
+        /*
+        var projectile = Instantiate(_projectile, _spawner.position, Quaternion.identity);
+        projectile.Init(dir * _projectileSpeed);
+        AudioSource.PlayClipAtPoint(_spawnClip, transform.position);
+        */
+
+        var projectile = Instantiate(_projectile, _spawner.position, Quaternion.LookRotation(dir, Vector3.up));
+        projectile.Init(dir * _projectileSpeed);
+
+        if (shootBack)
+        {
+            rb.AddForce(transform.forward * forceBack * -1, ForceMode.Impulse);
+        }
+    }
+
+    private IEnumerator ToggleLagIndicator()
+    {
+
+        _fired = true;
+        yield return new WaitForSeconds(0.2f);
+        _fired = false;
+
+    }
+
+
+}
